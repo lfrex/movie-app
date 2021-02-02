18e8392bcf453a1237c5f813f37b4d994fde884e
HectorU <urdanetahector@johndeere.com>
2021-02-02 11:39:24 -0500

    added code for index to users Controller and Routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,10 @@
 const users = require('../models/users');
 
-
+//Welcome page
+const index = (req, res) => {
+    res.render('users/index.ejs')
+};    
 
 module.exports = {
-        
+    index,        
 };
--- routes/users.js
+++ routes/users.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
+router.get('/', ctrl.users.index);
 
 module.exports = router;
