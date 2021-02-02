82f5fa78f1c044cd74f47b3854fe07774e279f07
villalpando <villalpandooctavio@johndeere.com>
2021-02-01 19:06:48 -0600

    Added some test data to movies.js model

---
+++ models/movies.js
@@ -0,0 +1,27 @@
+const movies = [
+    {
+        name: "The Godfather",
+        director:"Francis Ford Coppola",
+        year: 1972,
+        seenAlready: false
+    },
+    {
+        name: "Platoon",
+        director: "Oliver Stone",
+        year: 1986,
+        seenAlready: false
+    },
+    {
+        name: "The exorcist",
+        director: "William Friedkin",
+        year: 1973,
+        seenAlready: false
+    },
+    {
+        name: "Life of Brian",
+        director: "Terry Jones",
+        seenAlready: false
+    }
+];
+
+module.exports = movies;
