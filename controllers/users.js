2d348c887f869b1aa74a4b229cc5c4ca74d9b3c7
HectorU <urdanetahector@johndeere.com>
2021-02-02 12:07:12 -0500

    added signup code to controller, route and views ejs file

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,14 @@ const users = require('../models/users');
 //Welcome page
 const index = (req, res) => {
     res.render('users/index.ejs')
-};    
+};  
+//render signup page
+const renderSignup = (req, res) => {
+    res.render('users/signup.ejs')
+};   
+
 
 module.exports = {
-    index,        
+    index,
+    renderSignup,        
 };
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('../controllers');
 
+//route for welcome page
 router.get('/', ctrl.users.index);
+
+//route for signup page
+router.get('/signup', ctrl.users.renderSignup);
+
 
 module.exports = router;
