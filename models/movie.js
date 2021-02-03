e740a6d4b6680d5a07d3a69fb0d0313dc4e22c5c
villalpando <villalpandooctavio@johndeere.com>
2021-02-03 14:00:31 -0600

    Relationship between genres and movies stablished as one to many

--- models/movie.js
+++ models/movie.js
@@ -10,7 +10,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Movie.belongsTo(models.Genre, 
+        {
+          foreignKey: "genreId"
+        });
     }
   };
   Movie.init({
--- readme.md
+++ readme.md
@@ -78,7 +78,7 @@ The requirements for this project were to form a team with a partner to make a f
 <br>
 <br>
 
-## Techonologies Used
+## Technologies Used
 <br>
 
 
